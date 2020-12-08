7d7282b5b860d9b549190ce82fb204789867c9c9
Brandon Kruysman <brandon.kruysman@unity3d.com>
2020-12-08 10:50:52 -0800

    Add test sample

--- Tweening/Assets/test.cs
+++ Tweening/Assets/test.cs
@@ -14,21 +14,17 @@ public class test : MonoBehaviour
 
     ITweenEffect effect;
 
-    public void Scale()
+    public void Move()
     {
         if (cnt % 2 == 0)
         {
             effect = new MoveEffect(sphere, new Vector3(-5f,0f,0f), .5f, EaseType.CubicEaseOut);
             effect.OnComplete += OnEffectComplete;
-
-            //effect = new ScaleRectEffect(GetComponent<RectTransform>(), Vector3.one * 2f, .5f, EaseType.CubicEaseOut);
         }
         else
         {
             effect = new MoveEffect(sphere, new Vector3(5f,0f,0f), .5f, EaseType.CubicEaseOut);
             effect.OnComplete += OnEffectComplete;
-            
-            //effect = new ScaleRectEffect(GetComponent<RectTransform>(), Vector3.one, .25f, EaseType.CubicEaseOut);
         }
 
         cnt++;
